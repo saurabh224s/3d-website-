cd3c40ba35718ea40919e4df11b38b4668c776f2
Saurabh Pramod Ingale <112336045+saurabh224s@users.noreply.github.com>
2026-09-23 04:41:16 +0530

    Add AI Toolshelf project

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  sheet: "",
+  booksTab: "Books",
+  coloursTab: "Colours",
+  refreshSeconds: 15,
+  name: "The AI Toolshelf",
+  subtitle: "A shelf of AI tools worth knowing",
+  edition: "01"
+};
